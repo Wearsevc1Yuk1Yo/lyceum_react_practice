1c35cf7dafcfe7dba70b347a3ea0b16344b50231
Wearsevc1Yuk1Yo <bevgenia880@gmail.com>
2025-05-17 11:24:58 +0300

    links aside

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -1,11 +1,13 @@
+import { Link } from "react-router-dom";
+
 function Aside() {
     return(
       <aside>
         <div>
           <h2>Каталог</h2>
-          <p>материалы</p>
-          <p>безделушки</p>
-          <p>accessories</p>
+          <Link to="/category/materials"><p>материалы</p></Link>
+          <Link to="/category/trinkets"><p>безделушки</p></Link>
+          <Link to="/category/accessories"><p>accessories</p></Link>
         </div>
       </aside>
     );
