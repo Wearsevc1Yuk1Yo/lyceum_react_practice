a0412bad5b1e04e75fbd43f06307567a7f047dc3
Wearsevc1Yuk1Yo <bevgenia880@gmail.com>
2025-05-17 11:40:20 +0300

    Cart counter changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
 import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
 
 function Header() {
+
+    const { cart } = useContext(CartContext);
+    const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+
     return(
       <nav>
         <div className="navbar">
             <Link to="/">Главная</Link>
-            <Link to="/cart">Корзина</Link>
+            <Link to="/cart">Корзина ({totalItems}) </Link>
         </div>
       </nav>
     );
