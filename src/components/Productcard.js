0ba0fd711bcf03eafc45aca532a5ae67c0d649f0
Wearsevc1Yuk1Yo <bevgenia880@gmail.com>
2025-05-17 12:06:47 +0300

    i forgot

--- src/components/Productcard.js
+++ src/components/Productcard.js
@@ -1,13 +1,19 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+
 function ProductCard({name, desc, price, img}) {
+
+    const { addToCart } = useContext(CartContext);
+
     return(
         <div className="card">
             <div className="card-img">
-                <img src=""></img>
+                <img src={product.img} alt={product.name}></img>
             </div>
-            <p className="card-name">{name}</p>
-            <p className="card-desc">{desc}</p>
-            <p className="card-price">{price}</p>
-            <button>Добавить</button>
+            <p className="card-name">${product.name}</p>
+            <p className="card-desc">${product.desc}</p>
+            <p className="card-price">${product.price}</p>
+            <button onClick={() => addToCart(product)}>Добавить в корзину</button>
         </div>
     );
 }
